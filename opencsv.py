9f762c872a9f5f45cbd26dc1e372e89e2846df58
Anunay <anunay.j1@gmail.com>
2019-02-09 15:50:03 +0530

    Added CSV

---
+++ opencsv.py
@@ -0,0 +1,13 @@
+class opencsv:
+    def __init__(self,filename):
+        self.file = open(filename+".csv","a+")
+    def append(self,data):
+        for entry in data:
+            self.file.write(entry)
+            if entry is data[-1]:
+                self.file.write("\n")
+            else:
+                self.file.write(",")
+
+    def close(self):
+        self.file.close()
